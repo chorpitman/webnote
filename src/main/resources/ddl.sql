304ccce0f5478deee7daf97362e6fa2f7db74366
mac os chorpitman <chorpita@gmail.com>
2016-02-05 11:22:01 +0200

    edit ddl files

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -4,13 +4,13 @@ use notes1;
 
 CREATE TABLE Role
 (
-    id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    role VARCHAR(20) NOT NULL
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    role VARCHAR(20) NOT NULL UNIQUE
 );
 
 CREATE TABLE User
 (
-    id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     login VARCHAR(20) NOT NULL,
     password VARCHAR(20) NOT NULL,
     role_id INT(11) NOT NULL,
@@ -20,7 +20,7 @@ CREATE INDEX role_id ON User (role_id);
 
 CREATE TABLE Note
 (
-    id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     note_date DATETIME NOT NULL,
     title VARCHAR(70) NOT NULL,
     category VARCHAR(30) NOT NULL,
@@ -30,17 +30,17 @@ CREATE TABLE Note
 );
 CREATE INDEX user_id ON note (user_id);
 
-INSERT INTO `notes1`.`Role` (`id`, `role`) VALUES ('1', 'admin');
-INSERT INTO `notes1`.`Role` (`id`, `role`) VALUES ('2', 'user');
-INSERT INTO `notes1`.`Role` (`id`, `role`) VALUES ('3', 'superuser');
+INSERT INTO Role (role) VALUES ('admin');
+INSERT INTO Role (role) VALUES ('user');
+INSERT INTO Role (role) VALUES ('superuser');
 
-INSERT INTO `notes1`.`User` (`id`, `login`, `password`, `role_id`) VALUES ('1', 'admin', 'a', '1');
-INSERT INTO `notes1`.`User` (`id`, `login`, `password`, `role_id`) VALUES ('2', 'user', 'u', '2');
-INSERT INTO `notes1`.`User` (`id`, `login`, `password`, `role_id`) VALUES ('3', 'suser', 's', '3');
+INSERT INTO User (login, password, role_id) VALUES ('admin', 'a', 1);
+INSERT INTO User (login, password, role_id) VALUES ('user', 'u', 2);
+INSERT INTO User (login, password, role_id) VALUES ('suser', 's', 3);
 
-INSERT INTO `notes1`.`Note` (`id`, `note_date`, `title`, `category`, `description`, `user_id`) VALUES ('1', '2016-01-01', 'adminTitle', 'main', 'qazxcvwssdf', '1');
-INSERT INTO `notes1`.`Note` (`id`, `note_date`, `title`, `category`, `description`, `user_id`) VALUES ('2', '2016-01-02', 'adminTime', 'main', 'qazxcvwssdf', '1');
-INSERT INTO `notes1`.`Note` (`id`, `note_date`, `title`, `category`, `description`, `user_id`) VALUES ('3', '2016-01-03', 'adminTitle', 'general', 'qwe', '1');
-INSERT INTO `notes1`.`Note` (`id`, `note_date`, `title`, `category`, `description`, `user_id`) VALUES ('4', '2016-01-04', 'userTitle', 'general', 'qwe', '2');
-INSERT INTO `notes1`.`Note` (`id`, `note_date`, `title`, `category`, `description`, `user_id`) VALUES ('5', '2016-01-04', 'userTitle', 'general', 'qwe', '2');
-INSERT INTO `notes1`.`Note` (`id`, `note_date`, `title`, `category`, `description`, `user_id`) VALUES ('6', '2016-01-04', 'superUserTitle', 'general', 'qwe', '3');
+INSERT INTO Note (note_date, title, category, description, user_id) VALUES ('2016-01-01', 'adminTitle', 'main', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 1);
+INSERT INTO Note (note_date, title, category, description, user_id) VALUES ('2016-01-02', 'adminTime', 'main', 'Lorem ipsum dolor sit amet', 1);
+INSERT INTO Note (note_date, title, category, description, user_id) VALUES ('2016-01-03', 'adminTitle', 'general', 'Lorem ipsum dolor', 1);
+INSERT INTO Note (note_date, title, category, description, user_id) VALUES ('2016-01-04', 'userTitle', 'general', 'Lorem ipsum', 2);
+INSERT INTO Note (note_date, title, category, description, user_id) VALUES ('2016-01-04', 'userTitle', 'general', 'Lorem ipsum dol', 2);
+INSERT INTO Note (note_date, title, category, description, user_id) VALUES ('2016-01-04', 'superUserTitle', 'general', 'Lorem ipsum dolor sit a', 3);
